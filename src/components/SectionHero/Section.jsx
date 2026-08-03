4a3bc9727f833b608128b6464737e69094459124
Abdul Mannan <abdulmannan010spn@gmail.com>
2026-08-03 20:50:58 +0530

    Okay

--- src/components/SectionHero/Section.jsx
+++ src/components/SectionHero/Section.jsx
@@ -23,12 +23,12 @@ const Section = (props) => {
                <img src={props.sign} className="h-full w-auto object-contain mt-20" alt="" />
             </div>
            </div>
-                <div className="relative lg:bg-[#31d67b] bg-[#111923] lg:flex justify-center items-center">
+                <div className="relative lg:bg-[#31d67b] bg-[#111923] lg:flex justify-center items-center h-[120%]">
                   
                   <img 
                     src={props.profile2}
                     alt=""
-                    className="profile absolute lg:right-3/5 lg:h-[85.66666666667%] h-[190%] right-0.5 bottom-145 lg:top-1/7 md:bottom-121 md:h-full mr-5"
+                    className="profile absolute lg:right-3/5 lg:h-[85.66666666667%] h-[190%] right-0.5 bottom-147 lg:top-1/7 md:bottom-121 md:h-full mr-5"
                   />
                 </div>
               </section>
